38f9720d4b808017bb9e2ff9f6b843b4dfa60b73
Vadim <vad1mk4@bk.ru>
2023-08-06 17:06:12 +0300

    news_portal proj

--- News_Portal/news_portal/base/newsportal/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-# Импортируем созданное нами представление
-from .views import PostsList, PostDetail
-
-
-urlpatterns = [
-    path('', PostsList.as_view()),
-    path('<int:pk>', PostDetail.as_view()),
-]
